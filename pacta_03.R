4a3305ee792473650e4bb6bc4196bbc34900dad8
Alex Axthelm <aaxthelm@RMI.org>
2024-02-01 01:06:25 +0100

    Get portfolio parameters from config json

--- pacta_03.R
+++ pacta_03.R
@@ -35,7 +35,6 @@ if (!exists("portfolio_root_dir")) {
 
 setup_project()
 
-set_portfolio_parameters(file_path = file.path(par_file_path, paste0(portfolio_name_ref_all, "_PortfolioParameters.yml")))
 
 set_project_parameters(file.path(working_location, "parameter_files", paste0("ProjectParameters_", project_code, ".yml")))
 
@@ -54,7 +53,7 @@ if (file.exists(total_portfolio_path)) {
 
 # fix parameters ---------------------------------------------------------------
 
-if (project_code == "GENERAL") {
+if (cfg$project_code == "GENERAL") {
   language_select <- "EN"
 }
 
@@ -74,11 +73,11 @@ add_inv_and_port_names_if_needed <- function(data) {
   }
 
   if (!"portfolio_name" %in% names(data)) {
-    data <- mutate(data, portfolio_name = .env$portfolio_name, .before = everything())
+    data <- mutate(data, portfolio_name = cfg$portfolio_name, .before = everything())
   }
 
   if (!"investor_name" %in% names(data)) {
-    data <- mutate(data, investor_name = .env$investor_name, .before = everything())
+    data <- mutate(data, investor_name = cfg$investor_name, .before = everything())
   }
 
   data
@@ -145,22 +144,22 @@ bonds_results_map <- readRDS_or_return_alt_data(
 bonds_results_map <- add_inv_and_port_names_if_needed(bonds_results_map)
 
 peers_equity_results_portfolio <- readRDS_or_return_alt_data(
-  filepath = file.path(cfg$data_dir, paste0(project_code, "_peers_equity_results_portfolio.rds")),
+  filepath = file.path(cfg$data_dir, paste0(cfg$project_code, "_peers_equity_results_portfolio.rds")),
   alt_return = empty_portfolio_results()
 )
 
 peers_bonds_results_portfolio <- readRDS_or_return_alt_data(
-  filepath = file.path(cfg$data_dir, paste0(project_code, "_peers_bonds_results_portfolio.rds")),
+  filepath = file.path(cfg$data_dir, paste0(cfg$project_code, "_peers_bonds_results_portfolio.rds")),
   alt_return = empty_portfolio_results()
 )
 
 peers_equity_results_user <- readRDS_or_return_alt_data(
-  filepath = file.path(cfg$data_dir, paste0(project_code, "_peers_equity_results_portfolio_ind.rds")),
+  filepath = file.path(cfg$data_dir, paste0(cfg$project_code, "_peers_equity_results_portfolio_ind.rds")),
   alt_return = empty_portfolio_results()
 )
 
 peers_bonds_results_user <- readRDS_or_return_alt_data(
-  filepath = file.path(cfg$data_dir, paste0(project_code, "_peers_bonds_results_portfolio_ind.rds")),
+  filepath = file.path(cfg$data_dir, paste0(cfg$project_code, "_peers_bonds_results_portfolio_ind.rds")),
   alt_return = empty_portfolio_results()
 )
 
@@ -171,8 +170,8 @@ indices_bonds_results_portfolio <- readRDS(file.path(cfg$data_dir, "Indices_bond
 
 # create interactive report ----------------------------------------------------
 
-survey_dir <- file.path(user_results_path, project_code, "survey")
-real_estate_dir <- file.path(user_results_path, project_code, "real_estate")
+survey_dir <- file.path(user_results_path, cfg$project_code, "survey")
+real_estate_dir <- file.path(user_results_path, cfg$project_code, "real_estate")
 output_dir <- file.path(cfg$output_dir)
 dataframe_translations <- readr::read_csv(
   system.file("extdata/translation/dataframe_labels.csv", package = "pacta.portfolio.report"),
@@ -195,7 +194,6 @@ sector_order <- readr::read_csv(
 
 # combine config files to send to create_interactive_report()
 portfolio_config_path <- file.path(par_file_path, paste0(portfolio_name_ref_all, "_PortfolioParameters.yml"))
-project_config_path <- file.path(working_location, "parameter_files", paste0("ProjectParameters_", project_code, ".yml"))
 pacta_data_public_manifest <-
   list(
     creation_time_date = jsonlite::read_json(file.path(cfg$data_dir, "manifest.json"))$creation_time_date,
@@ -213,7 +211,7 @@ configs <-
 class(configs$portfolio_config) <- c(class(configs$portfolio_config), "list")
 class(configs$project_config) <- c(class(configs$project_config), "list")
 
-template_dir_name <- paste(tolower(project_report_name), tolower(language_select), "template", sep = "_")
+template_dir_name <- paste(tolower(project_report_name), tolower(cfg$language_select), "template", sep = "_")
 template_dir <- file.path(template_path, template_dir_name)
 
 create_interactive_report(
@@ -221,10 +219,10 @@ create_interactive_report(
   output_dir = output_dir,
   survey_dir = survey_dir,
   real_estate_dir = real_estate_dir,
-  language_select = language_select,
-  investor_name = investor_name,
-  portfolio_name = portfolio_name,
-  peer_group = peer_group,
+  language_select = cfg$language_select,
+  investor_name = cfg$investor_name,
+  portfolio_name = cfg$portfolio_name,
+  peer_group = cfg$peer_group,
   start_year = start_year,
   select_scenario = select_scenario,
   select_scenario_other = scenario_other,
@@ -261,26 +259,26 @@ create_interactive_report(
 
 # create executive summary -----------------------------------------------------
 
-survey_dir <- fs::path_abs(file.path(user_results_path, project_code, "survey"))
-real_estate_dir <- fs::path_abs(file.path(user_results_path, project_code, "real_estate"))
-score_card_dir <- fs::path_abs(file.path(user_results_path, project_code, "score_card"))
+survey_dir <- fs::path_abs(file.path(user_results_path, cfg$project_code, "survey"))
+real_estate_dir <- fs::path_abs(file.path(user_results_path, cfg$project_code, "real_estate"))
+score_card_dir <- fs::path_abs(file.path(user_results_path, cfg$project_code, "score_card"))
 output_dir <- file.path(cfg$output_dir)
 es_dir <- file.path(output_dir, "executive_summary")
 if (!dir.exists(es_dir)) {
   dir.create(es_dir, showWarnings = FALSE, recursive = TRUE)
 }
 
-exec_summary_template_name <- paste0(project_code, "_", tolower(language_select), "_exec_summary")
+exec_summary_template_name <- paste0(cfg$project_code, "_", tolower(cfg$language_select), "_exec_summary")
 exec_summary_builtin_template_path <- system.file("extdata", exec_summary_template_name, package = "pacta.executive.summary")
 invisible(file.copy(exec_summary_builtin_template_path, output_dir, recursive = TRUE, copy.mode = FALSE))
 exec_summary_template_path <- file.path(output_dir, exec_summary_template_name)
 
-if (dir.exists(exec_summary_template_path) && (peer_group %in% c("assetmanager", "bank", "insurance", "pensionfund"))) {
+if (dir.exists(exec_summary_template_path) && (cfg$peer_group %in% c("assetmanager", "bank", "insurance", "pensionfund"))) {
   data_aggregated_filtered <-
     prep_data_executive_summary(
-      investor_name = investor_name,
-      portfolio_name = portfolio_name,
-      peer_group = peer_group,
+      investor_name = cfg$investor_name,
+      portfolio_name = cfg$portfolio_name,
+      peer_group = cfg$peer_group,
       start_year = start_year,
       scenario_source = "GECO2021",
       scenario_selected = "1.5C-Unif",
@@ -318,7 +316,7 @@ if (dir.exists(exec_summary_template_path) && (peer_group %in% c("assetmanager",
 
   render_executive_summary(
     data = data_aggregated_filtered,
-    language = language_select,
+    language = cfg$language_select,
     output_dir = es_dir,
     exec_summary_dir = exec_summary_template_path,
     survey_dir = survey_dir,
@@ -326,9 +324,9 @@ if (dir.exists(exec_summary_template_path) && (peer_group %in% c("assetmanager",
     real_estate_flag = real_estate_flag,
     score_card_dir = score_card_dir,
     file_name = "template.Rmd",
-    investor_name = investor_name,
-    portfolio_name = portfolio_name,
-    peer_group = peer_group,
+    investor_name = cfg$investor_name,
+    portfolio_name = cfg$portfolio_name,
+    peer_group = cfg$peer_group,
     total_portfolio = total_portfolio,
     scenario_selected = "1.5C-Unif",
     currency_exchange_value = currency_exchange_value,
