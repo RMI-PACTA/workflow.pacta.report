f90ad2e1d322f64bf4d72f870e321f5e4bfbb2a7
Alex Axthelm <aaxthelm@RMI.org>
2024-05-29 21:24:51 +0200

    Call an argumentless function as entrypoint

--- run-pacta.sh
+++ run-pacta.sh
@@ -3,4 +3,4 @@
 # Set permissions so that new files can be deleted/overwritten outside docker
 umask 000
 
-Rscript --vanilla /workflow.pacta.report/pacta_03.R "${1}" \
+Rscript --vanilla -e "workflow.pacta.report:::run_pacta_reporting_process()" "${1}" \
