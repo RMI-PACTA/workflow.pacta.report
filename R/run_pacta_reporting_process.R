8d7fba448869aa3778c8578a73a5c3bd5d88c4ad
Alex Axthelm <aaxthelm@RMI.org>
2024-05-31 19:21:20 +0200

    Fix call to real estate dir

--- R/run_pacta_reporting_process.R
+++ R/run_pacta_reporting_process.R
@@ -5,7 +5,8 @@ run_pacta_reporting_process <- function(
   report_output_dir = Sys.getenv("REPORT_OUTPUT_DIR"),
   summary_output_dir = Sys.getenv("SUMMARY_OUTPUT_DIR"),
   real_estate_dir = Sys.getenv("REAL_ESTATE_DIR"),
-  survey_dir = Sys.getenv("SURVEY_DIR")
+  survey_dir = Sys.getenv("SURVEY_DIR"),
+  score_card_dir = Sys.getenv("SCORE_CARD_DIR")
 ) {
   log_debug("Checking configuration.")
   if (is.null(analysis_output_dir) || analysis_output_dir == "") {
@@ -250,8 +251,8 @@ run_pacta_reporting_process <- function(
   prepare_interactive_report(
     report_output_dir = report_output_dir,
     project_report_name = params[["project_report_name"]],
-    survey_dir = params[["survey_dir"]],
-    real_estate_dir = params[["real_estate_dir"]],
+    survey_dir = survey_dir,
+    real_estate_dir = real_estate_dir,
     language_select = params[["language_select"]],
     peer_group = params[["peer_group"]],
     investor_name = params[["investor_name"]],
@@ -288,9 +289,9 @@ run_pacta_reporting_process <- function(
   # create executive summary -----------------------------------------------------
   prepare_executive_summary(
     summary_output_dir = summary_output_dir,
-    survey_dir = params[["survey_dir"]],
-    real_estate_dir = params[["real_estate_dir"]],
-    score_card_dir = params[["score_card_dir"]],
+    survey_dir = survey_dir,
+    real_estate_dir = real_estate_dir,
+    score_card_dir = score_card_dir,
     project_code = params[["project_code"]],
     language_select = params[["language_select"]],
     peer_group = params[["peer_group"]],
