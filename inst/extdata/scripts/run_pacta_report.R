de299338cc3c48633b74543f5c3431abdf180db0
Alex Axthelm <alex@alexaxthelm.com>
2024-11-04 12:05:40 +0100

    Parse params in script (#30)
    * Use array of file names for portfolio, not string
    
    * Add imported functions to globals, supress `lintr`
    
    * Parse parameters in script, not function
    
    * Dev version bump
    
    * Update Testing configs
    
    * Remove `jsonvalidate` dependency
    
    * Remove pacta.workflow.utils dependency
    
    * Check output directories are writable
    
    * Fix namespace for function
    
    * export report manifest
    
    * Linting
    
    * Fileter input files to those that exist
    
    * Use RMI action for blob upload.
    
    * Trigger CI

--- inst/extdata/scripts/run_pacta_report.R
+++ inst/extdata/scripts/run_pacta_report.R
@@ -1,2 +1,30 @@
-logger::log_threshold(Sys.getenv("LOG_LEVEL", "WARN"))
-workflow.pacta.report:::run_pacta_reporting_process(commandArgs(trailingOnly = TRUE))
+logger::log_threshold(Sys.getenv("LOG_LEVEL", "INFO"))
+
+raw_params <- commandArgs(trailingOnly = TRUE)
+params <- pacta.workflow.utils::parse_raw_params(
+  json = raw_params,
+  inheritence_search_paths = system.file(
+    "extdata", "parameters",
+    package = "workflow.pacta.report"
+  ) ,
+  schema_file = system.file(
+    "extdata", "schema", "reportingParameters.json",
+    package = "workflow.pacta.report"
+  ),
+  raw_schema_file = system.file(
+    "extdata", "schema", "rawParameters.json",
+    package = "workflow.pacta.report"
+  )
+)
+
+manifest_info <- workflow.pacta.report:::run_pacta_reporting_process(
+  params = params
+)
+
+pacta.workflow.utils::export_manifest(
+  input_files = manifest_info[["input_files"]],
+  output_files = manifest_info[["output_files"]],
+  params = manifest_info[["params"]],
+  manifest_path = file.path(Sys.getenv("REPORT_OUTPUT_DIR"), "manifest.json"),
+  raw_params = raw_params
+)
